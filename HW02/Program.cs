28b4e3cb34991ca800fe4ff7f7ee63a0bff96433
GeethanjaliSangu <54648522+GeethanjaliSangu@users.noreply.github.com>
2019-09-12 16:05:20 -0500

    Add files via upload

---
+++ HW02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first Integer");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second Integer");
+            int num2 = int.Parse(Console.ReadLine());
+            if (num1 == num2)
+            {
+                Console.WriteLine("Both the numbers are same");
+            }
+            else if (num1 > num2)
+            {
+                Console.WriteLine(num1 + "is larger");
+            }
+            else
+            {
+                Console.WriteLine(num2 + "is larger");
+            }
+            Console.ReadLine();
+        }
+    }
+}
