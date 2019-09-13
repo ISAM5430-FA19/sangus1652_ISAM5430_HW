38d35bb923fa7644c68483f5744c3b59bdf8d99d
borismo1992 <51873297+borismo1992@users.noreply.github.com>
2019-09-12 21:45:53 -0500

    Add files via upload

---
+++ HW02/Sum_Average/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the count of numbers");
+            int count = int.Parse(Console.ReadLine());
+            int n = 0;
+            int sum = 0;
+            float average = 0;
+            for (int i = 1; i<=count; i++)
+            {
+                n=int.Parse(Console.ReadLine());
+                sum = sum + n;     
+            }
+            Console.WriteLine(sum);
+            average =(float) (sum) / (count);
+            Console.WriteLine(average);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HW02/TwoDigit_IntegerSquares/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDigit_IntegerSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 1; i <= 9; i++)
+            {
+                int num = i * i;
+                if (num > 10 && num < 99)
+                {
+                    Console.WriteLine(num);
+                }
+                            
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
